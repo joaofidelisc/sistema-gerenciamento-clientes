b17dfb9928f6d21dc076c9f16de65f16ca683263
joao.fidelis51 <joao.fidelis51@gmail.com>
2024-03-07 22:41:18 -0300

    refactor: remoção de imports não utilizados

--- frontend/src/pages/CadastroCliente/index.js
+++ frontend/src/pages/CadastroCliente/index.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "../../redux/slices/userSlice";
 import { useForm } from "react-hook-form";
 import NavBar from "../../components/NavBar.js";
 import { toast, ToastContainer } from "react-toastify";
@@ -11,17 +8,11 @@ import "react-toastify/dist/ReactToastify.css";
 import "./styles.css";
 
 function CadastroCliente() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const users = useSelector((state) => state.user.users);
-  const currentUser = useSelector((state) => state.user.currentUser);
 
   const toastHandler = (message, status) => {
     if (status === "success")
